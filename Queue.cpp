6e8a087a9401e4d796038357eef4d5d2a035d931
Aashis Rijal <aashisrijal252@gmail.com>
2024-01-18 17:41:20 +0545

    update queue

--- Queue.cpp
+++ Queue.cpp
@@ -11,14 +11,23 @@ struct queue {
 };
 typedef struct queue qt;
 
+// check empty
 int Isempty(qt *q) {
     return (q->rear < q->front);
 }
 
+// check full 
 int Isfull(qt *q) {
     return (q->rear == N-1);
 }
 
+// made empty thw queue function 
+void madeEmpty(qt *q){
+	q->front = 0;
+	q->rear = -1;
+}
+
+//enqueue function
 void Enqueue(qt *q) {
     int data; 
     if (Isfull(q)) {
@@ -32,29 +41,49 @@ void Enqueue(qt *q) {
     }
 }
 
-
+//dequeue function
 void Dequeue(qt *q) {
     int data;
     if (Isempty(q)) {
         printf("\nThere is no data to Dequeue\n");
     } else {
-        data = q->items[q->front++];
+        data=q->items[q->front];
+        q->front++;
         printf("\nDequeued item is: %d\n", data);
     }
 }
 
+// display function
+void show(qt *q){
+	if(!Isempty(q)){
+		for(int i=q->front;i<=q->rear;i++){
+			printf("| %d | \n",q->items[i]);
+		}
+	}else{
+		printf("\n Sorry  you have no data to show! \n");
+	}
+}
 
+//display front function
+void front(qt *q){
+	if(!Isempty(q)){
+		printf("\n The value at front is %d", q->items[q->front]);
+	}else{
+		printf("\nSorry you have no data\n");
+	}
+}
+
+// main program goes here
 int main() {
-    qt queue;
-    queue.front = 0;
-	queue.rear= -1; //  top to -1
-
+    qt q;
     int n;
     int data;
     int choice;
     char name[10];
     printf("\n ||Enter your name to start action:|| \n");
     scanf("%s",&name);
+    //initialllize the top and front
+    madeEmpty(&q);
     do{
     	 system("CLS");
     	 printf(" \n \t\t ****** Dear %s you are welcome **********\n",name);
@@ -62,6 +91,9 @@ int main() {
     printf("1. \t Dear %s Enter 1 for Enqueue the data:\n",name);
     printf("2. \t Dear %s Enter 2 for Dequeue the data: \n",name);
     printf("3. \t Dear %s Enter 3 to see the Queue data: \n",name);
+    printf("4. \t Dear %s Enter 4 to see front data: \n",name);
+    printf("5. \t Dear %s Enter 5 to clear the queue: \n",name);
+    printf("6. \t Dear %s Enter 6 to exit: \n",name);
     scanf("%d", &n);
     system("CLS");
 
@@ -70,26 +102,35 @@ int main() {
         	 system("CLS");
         	 printf(" \n \t\t ****** Dear %s you can perform Push Operation **********\n",name);
             printf("\n======= Pushing the data onto the Queue =======\n");
-            Enqueue(&queue);
+            Enqueue(&q);
             break;
             
         case 2:
         	 system("CLS");
         	printf(" \n \t\t ****** Dear %s you can perform Pop Operation **********\n",name);
             printf("\n======= Dequque the data from the Queue =======\n");
-            Dequeue(&queue);
+            Dequeue(&q);
             break;
             
         case 3:
         	 system("CLS");
-        	 printf(" \n \t\t ****** Dear %s your Queue data are as follow **********\n",name);
+        	 printf(" \n \t\t ****** %s your Queue data are as follow **********\n",name);
         	printf("===Displaying queue data:=====\n");
-        	for(int i=0;i<=queue.front;i++){
-        		printf("%d. %d \n",i,queue.items[i]);
-			}
+        	show(&q);
         	break;
         	
-        default:
+        case 4:
+        	system("CLS");
+        	printf(" \n \t\t ****** Dear %s your Queue front data are as follow **********\n",name);
+        	front(&q);
+       	    break;
+       	   
+       	   case 5:
+       	    	system("ClS");
+       	   	 	madeEmpty(&q);
+       	   		printf(" \n \t\t ****** %s you clear the QUEUE **********\n",name);
+       	   		break;
+        	default:
         	system("CLS");
         	printf(" \n \t\t ****** Dear %s  **********\n",name);
             printf("Please enter a valid instruction:\n");
